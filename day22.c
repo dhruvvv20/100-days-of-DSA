1718dce2e7079c433c1d0c15d445e5922a073edd
dhruvvv20 <dhruvchaurasia042@gmail.com>
2026-02-22 16:23:57 +0530

    added day 22

---
+++ day22.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void traverseAndPrint(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int countNodes(struct Node* head) {
+    int count = 0;
+    struct Node* temp = head;
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    struct Node* head = NULL;
+    for (int i = 0; i < n; i++) {
+        int data;
+        scanf("%d", &data);
+        insertAtEnd(&head, data);
+    }
+    traverseAndPrint(head);
+    printf("%d\n", countNodes(head));
+    // Free memory if needed, but for this problem, not necessary
+    return 0;
+}
