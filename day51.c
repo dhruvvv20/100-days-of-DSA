c17186a0aa8918e3a9779994e3644237ddedf83b
dhruvvv20 <dhruvchaurasia042@gmail.com>
2026-03-23 21:26:01 +0530

    added day 51

---
+++ day51.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+// Find LCA
+struct Node* findLCA(struct Node* root, int n1, int n2) {
+    while (root != NULL) {
+        if (n1 < root->data && n2 < root->data)
+            root = root->left;
+        else if (n1 > root->data && n2 > root->data)
+            root = root->right;
+        else
+            return root; // This is LCA
+    }
+    return NULL;
+}
+
+int main() {
+    int n, i, value, n1, n2;
+    struct Node* root = NULL;
+
+    // Input
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &value);
+        root = insert(root, value);
+    }
+
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* lca = findLCA(root, n1, n2);
+
+    if (lca != NULL)
+        printf("%d\n", lca->data);
+
+    return 0;
+}
